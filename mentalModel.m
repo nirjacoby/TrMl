24ed7364b9814a651b1c17528130f77d66054842
nirjacoby <nirjac@hotmail.com>
2016-06-02 13:10:06 -0400

    Add files via upload

--- mentalModel.m
+++ mentalModel.m
@@ -5,7 +5,7 @@ function [helpR, hindR, action, outcome] = mentalModel(trait,context)
     outcome = computeObservation(action);
 end    
 
-function [helpR, hindR] = computeState(trait,context)
+function [helpR, hindR] = computeState1(trait,context) %helper or hinderer, ignores context
 %    drawB = @(a,b) random(makedist('beta','a',a,'b',b));
 %    drawB2 = @(a,b) betainv(rand,a,b);
 
@@ -16,6 +16,29 @@ function [helpR, hindR] = computeState(trait,context)
         helpR = betainv(rand, 1,4); %drawB(1,4);
         hindR = betainv(rand, 4,1); %drawB(4,1);
     end
+end
+
+function [helpR, hindR] = computeState2(trait,context) %helps family and friends, ignores trait
+%    drawB = @(a,b) random(makedist('beta','a',a,'b',b));
+%    drawB2 = @(a,b) betainv(rand,a,b);
+
+    if context(1) <= 2 % relation is family or firend
+        helpR = betainv(rand, 4,1); %drawB(4,1);
+        hindR = betainv(rand, 1,4); %drawB(1,4);
+    else 
+        helpR = betainv(rand, 1,4); %drawB(1,4);
+        hindR = betainv(rand, 4,1); %drawB(4,1);
+    end
+end
+
+function [helpR, hindR] = computeState3(trait,context) % helps family and friends, ignores trait
+    helpscore = 1; %at baseline, act randomly, trait and context can up modulate propensity to help
+    helpscore = helpscore + (2 - trait); % add 1 points for helper, 0 for hinderer
+    helpscore = helpscore + 0.5*(3 - context(1)); % add 1 points for family, 0.5 for friend and 0 for stranger
+    helpscore = helpscore + 0.5*(context(2) - 1); % add 0.5 points for female, 0 for male
+    
+    helpR = betainv(rand, helpscore, 1); %drawB(4,1);
+    hindR = betainv(rand, 1, helpscore); %drawB(1,4);
 end
 
 function intent = computeIntent(helpR, hindR)
@@ -30,7 +53,7 @@ function intent = computeIntent(helpR, hindR)
 end
 
 function outcome = computeObservation(action)
-    competence = 1;
+    competence = 1; %noise parameter
     outcome = rand<competence;
     if outcome
         outcome = action;
